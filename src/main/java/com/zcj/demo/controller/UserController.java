187311e0acead05a856fec4ba1c567de625a0ac4
zengchengjie <zengchengjieit@163.com>
2018-09-07 17:52:52 +0800

    添加注册登录，修改banner

--- src/main/java/com/zcj/demo/controller/UserController.java
+++ src/main/java/com/zcj/demo/controller/UserController.java
@@ -8,10 +8,7 @@ import com.zcj.demo.dao.UserMapper;
 import com.zcj.demo.model.User;
 import com.zcj.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.io.Serializable;
@@ -35,5 +32,16 @@ public class UserController {
     public String Test(){
         String name  = EnumTest.SPRING.getName();
         return name;
+    }
+    @PostMapping("/login")
+    public Result login(@RequestParam String username,
+                        @RequestParam String password){
+        return ResultGenerator.genSuccessResult();
+
+    }
+    @PostMapping("/register")
+    public Result register(@RequestParam String username,
+                           @RequestParam String password){
+        return ResultGenerator.genSuccessResult();
     }
 }
