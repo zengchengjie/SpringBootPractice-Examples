acd01373d879fe4efe9fe2a332fa6cf858f5c43f
zengchengjie <zengchengjieit@163.com>
2018-12-13 16:42:59 +0800

    fix bugs

--- src/main/java/com/zcj/demo/core/shiro/MyShiroRealm.java
+++ src/main/java/com/zcj/demo/core/shiro/MyShiroRealm.java
@@ -1,15 +1,13 @@
 package com.zcj.demo.core.shiro;
 
-import com.xxx.shiro.entity.SysPermission;
-import com.xxx.shiro.entity.SysRole;
-import com.zcj.demo.model.User;
 import com.zcj.demo.service.UserService;
-import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.util.ByteSource;
 
 import javax.annotation.Resource;
 
@@ -26,13 +24,13 @@ public class MyShiroRealm extends AuthorizingRealm {
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
 //        System.out.println("权限配置-->MyShiroRealm.doGetAuthorizationInfo()");
         SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
-        User userInfo = (User) principals.getPrimaryPrincipal();
+       /* User userInfo = (User) principals.getPrimaryPrincipal();
         for (SysRole role : userInfo.getRoleList()) {
             authorizationInfo.addRole(role.getRole());
             for (SysPermission p : role.getPermissions()) {
                 authorizationInfo.addStringPermission(p.getPermission());
             }
-        }
+        }*/
         return authorizationInfo;
     }
 
@@ -42,7 +40,7 @@ public class MyShiroRealm extends AuthorizingRealm {
             throws AuthenticationException {
 //        System.out.println("MyShiroRealm.doGetAuthenticationInfo()");
         //获取用户的输入的账号.
-        String username = (String) token.getPrincipal();
+        /*String username = (String) token.getPrincipal();
 //        System.out.println(token.getCredentials());
         //通过username从数据库中查找 User对象，如果找到，没找到.
         //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
@@ -60,7 +58,8 @@ public class MyShiroRealm extends AuthorizingRealm {
                 ByteSource.Util.bytes(userInfo.getCredentialsSalt()),//salt=username+salt
                 getName()  //realm name
         );
-        return authenticationInfo;
+        return authenticationInfo;*/
+        return null;
     }
 
 }
--- src/main/java/com/zcj/demo/core/shiro/ShiroConfig.java
+++ src/main/java/com/zcj/demo/core/shiro/ShiroConfig.java
@@ -13,7 +13,7 @@ import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerExceptionResolver;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -112,7 +112,7 @@ public class ShiroConfig {
         RedisManager redisManager = new RedisManager();
         redisManager.setHost(host);
         redisManager.setPort(port);
-        redisManager.setExpire(1800);// 配置缓存过期时间
+//        redisManager.setExpire(1800);// 配置缓存过期时间
         redisManager.setTimeout(timeout);
         redisManager.setPassword(password);
         return redisManager;
@@ -162,8 +162,8 @@ public class ShiroConfig {
      * 注册全局异常处理
      * @return
      */
-    @Bean(name = "exceptionHandler")
-    public HandlerExceptionResolver handlerExceptionResolver() {
-        return new MyExceptionHandler();
-    }
+//    @Bean(name = "exceptionHandler")
+//    public HandlerExceptionResolver handlerExceptionResolver() {
+//        return new MyExceptionHandler();
+//    }
 }
