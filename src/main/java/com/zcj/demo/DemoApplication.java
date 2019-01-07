4498a621d0334c955b225581acd62ac91a3814e1
zengchengjie <zengchengjieit@163.com>
2019-01-07 16:50:09 +0800

    修改配置文件，修复运行项目失败（At least one JPA metamodel must be present）的问题

--- src/main/java/com/zcj/demo/DemoApplication.java
+++ src/main/java/com/zcj/demo/DemoApplication.java
@@ -3,10 +3,11 @@ package com.zcj.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,JpaRepositoriesAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
