5706c3ca8d0916a27d0580a8281c6925fe8c45dd
zengchengjie <zengchengjieit@163.com>
2019-04-28 16:51:57 +0800

    修改注释

--- src/main/java/com/zcj/demo/ioctest/xml/XmlBeanDefinitionReader.java
+++ src/main/java/com/zcj/demo/ioctest/xml/XmlBeanDefinitionReader.java
@@ -15,129 +15,129 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 
 /**
- * ����XML�ļ�
+ * 解析XML文件
  *
  * @author stateis0
  */
 public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
 
   /**
-   * ���������������һ����Դ������
+   * 构造器，必须包含一个资源加载器
    *
-   * @param resourceLoader ��Դ������
+   * @param resourceLoader 资源加载器
    */
   public XmlBeanDefinitionReader(ResourceLoader resourceLoader) {
     super(resourceLoader);
   }
 
   public void readerXML(String location) throws Exception {
-    // ����һ����Դ������
+    // 创建一个资源加载器
     ResourceLoader resourceloader = new ResourceLoader();
-    // ����Դ�������л�ȡ������
+    // 从资源加载器中获取输入流
     InputStream inputstream = resourceloader.getResource(location).getInputstream();
-    // ��ȡ�ĵ������߹���ʵ��
+    // 获取文档建造者工厂实例
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    // ���������ĵ�������
+    // 工厂创建文档建造者
     DocumentBuilder docBuilder = factory.newDocumentBuilder();
-    // �ĵ������߽����� �����ĵ�����
+    // 文档建造者解析流 返回文档对象
     Document doc = docBuilder.parse(inputstream);
-    // ���ݸ������ĵ�������н�������ע�ᵽbean������
+    // 根据给定的文档对象进行解析，并注册到bean容器中
     registerBeanDefinitions(doc);
-    // �ر���
+    // 关闭流
     inputstream.close();
   }
 
   /**
-   * ���ݸ������ĵ�������н�������ע�ᵽbean������
+   * 根据给定的文档对象进行解析，并注册到bean容器中
    *
-   * @param doc �ĵ�����
+   * @param doc 文档对象
    */
   private void registerBeanDefinitions(Document doc) {
-    // ��ȡ�ĵ��ĸ�Ԫ��
+    // 读取文档的根元素
     Element root = doc.getDocumentElement();
-    // ����Ԫ�صĸ��ڵ㼰���ڵ��µ������ӽڵ㲢��ӽ�ע������
+    // 解析元素的根节点及根节点下的所有子节点并添加进注册容器
     parseBeanDefinitions(root);
   }
 
   /**
-   * ����Ԫ�صĸ��ڵ㼰���ڵ��µ������ӽڵ㲢��ӽ�ע������
+   * 解析元素的根节点及根节点下的所有子节点并添加进注册容器
    *
-   * @param root XML �ļ����ڵ�
+   * @param root XML 文件根节点
    */
   private void parseBeanDefinitions(Element root) {
-    // ��ȡ��Ԫ�ص�������Ԫ��
+    // 读取根元素的所有子元素
     NodeList nl = root.getChildNodes();
-    // ������Ԫ��
+    // 遍历子元素
     for (int i = 0; i < nl.getLength(); i++) {
-      // ��ȡ��Ԫ�صĸ���λ�õĽڵ�
+      // 获取根元素的给定位置的节点
       Node node = nl.item(i);
-      // �����ж�
+      // 类型判断
       if (node instanceof Element) {
-        // ǿתΪ������Ԫ��
+        // 强转为父类型元素
         Element ele = (Element) node;
-        // �����������Ľڵ㣬����name��class��property�� name�� value��ref
+        // 解析给给定的节点，包括name，class，property， name， value，ref
         processBeanDefinition(ele);
       }
     }
   }
 
   /**
-   * �����������Ľڵ㣬����name��class��property�� name�� value��ref
+   * 解析给给定的节点，包括name，class，property， name， value，ref
    *
-   * @param ele XML ����Ԫ��
+   * @param ele XML 解析元素
    */
   private void processBeanDefinition(Element ele) {
-    // ��ȡ����Ԫ�ص� name ����
+    // 获取给定元素的 name 属性
     String name = ele.getAttribute("name");
-    // ��ȡ����Ԫ�ص� class ����
+    // 获取给定元素的 class 属性
     String className = ele.getAttribute("class");
-    // ����һ��bean�������
+    // 创建一个bean定义对象
     BeanDefinition beanDefinition = new BeanDefinition();
-    // ����bean �������� ȫ�޶�����
+    // 设置bean 定义对象的 全限定类名
     beanDefinition.setClassname(className);
-    // �� bean ע�������ļ��еĳ�Ա����
+    // 向 bean 注入配置文件中的成员变量
     addPropertyValues(ele, beanDefinition);
-    // ��ע������ ���bean���ƺ�bean����
+    // 向注册容器 添加bean名称和bean定义
     getRegistry().put(name, beanDefinition);
   }
 
   /**
-   * ��������ļ��е�����Ԫ�ص�bean����ʵ����
+   * 添加配置文件中的属性元素到bean定义实例中
    *
-   * @param ele Ԫ��
-   * @param beandefinition bean���� ����
+   * @param ele 元素
+   * @param beandefinition bean定义 对象
    */
   private void addPropertyValues(Element ele, BeanDefinition beandefinition) {
-    // ��ȡ����Ԫ�ص� property ���Լ���
+    // 获取给定元素的 property 属性集合
     NodeList propertyNode = ele.getElementsByTagName("property");
-    // ѭ������
+    // 循环集合
     for (int i = 0; i < propertyNode.getLength(); i++) {
-      // ��ȡ������ĳ������λ�õĽڵ�
+      // 获取集合中某个给定位置的节点
       Node node = propertyNode.item(i);
-      // �����ж�
+      // 类型判断
       if (node instanceof Element) {
-        // ���ڵ�����ǿתΪ��Ԫ��
+        // 将节点向下强转为子元素
         Element propertyEle = (Element) node;
-        // Ԫ�ض����ȡ name ����
+        // 元素对象获取 name 属性
         String name = propertyEle.getAttribute("name");
-        // Ԫ�ض����ȡ value ����ֵ
+        // 元素对象获取 value 属性值
         String value = propertyEle.getAttribute("value");
-        // �ж�value��Ϊ��
+        // 判断value不为空
         if (value != null && value.length() > 0) {
-          // ������� ��bean���塱 ʵ������Ӹó�Ա����
+          // 向给定的 “bean定义” 实例中添加该成员变量
           beandefinition.getPropertyValues().addPropertyValue(new PropertyValue(name, value));
         } else {
-          // ���Ϊ�գ����ȡ����ref
+          // 如果为空，则获取属性ref
           String ref = propertyEle.getAttribute("ref");
           if (ref == null || ref.length() == 0) {
-            // �������refΪ�գ����׳��쳣
+            // 如果属性ref为空，则抛出异常
             throw new IllegalArgumentException(
-                "Configuration problem: <property> element for property '"
-                    + name + "' must specify a ref or value");
+                    "Configuration problem: <property> element for property '"
+                            + name + "' must specify a ref or value");
           }
-          // �����Ϊ�գ��ⴴ��һ�� ��bean�����á� ʵ�����������Ϊ���ƣ�ʵ����ʱΪ��
+          // 如果不为空，测创建一个 “bean的引用” 实例，构造参数为名称，实例暂时为空
           BeanReference beanRef = new BeanReference(name);
-          // ������� ��bean���塱 ����ӳ�Ա����
+          // 向给定的 “bean定义” 中添加成员变量
           beandefinition.getPropertyValues().addPropertyValue(new PropertyValue(name, beanRef));
         }
       }
