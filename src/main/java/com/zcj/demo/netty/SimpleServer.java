0d821b3298d2a3fbb3ebcac2f465d7bbbcd6a1f1
zengchengjie <zengchengjieit@163.com>
2019-05-09 15:19:50 +0800

    添加一个简单的netty测试方法。

---
+++ src/main/java/com/zcj/demo/netty/SimpleServer.java
@@ -0,0 +1,49 @@
+package com.zcj.demo.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @Auther: 10062376
+ * @Date: 2019/5/9 15:09
+ * @Description:
+ */
+public class SimpleServer {
+    static int port = 8001;
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast(new SimpleServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            System.out.println("netty server 已启动");
+
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // 等待服务器  socket 关闭
+            f.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+
+            System.out.println("netty server 已关闭");
+        }
+    }
+}
---
+++ src/main/java/com/zcj/demo/netty/SimpleServerHandler.java
@@ -0,0 +1,33 @@
+package com.zcj.demo.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @Auther: 10062376
+ * @Date: 2019/5/9 15:09
+ * @Description:
+ */
+public class SimpleServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        byte[] bytes = new byte[in.readableBytes()];
+        in.readBytes(bytes);
+        System.out.println("收到消息:" + new String(bytes));
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("已连接:" + ctx.channel().remoteAddress());
+        ctx.fireChannelRegistered();
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("已断开:" + ctx.channel().remoteAddress());
+        ctx.fireChannelUnregistered();
+    }
+
+}
